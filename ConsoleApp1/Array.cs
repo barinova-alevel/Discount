fb9d8a0b80bed9f2b4273ccadd880ee2f1b25afd
Oksana <my@mail.com>
2018-07-26 23:20:15 +0300

    Add project files.

---
+++ ConsoleApp1/Array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Array
+    {
+        public void ArrayOutput()
+        {
+            int[,] arrayOut
+                = new int[5, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 0, 1, 2, 3, 4 }, { 10, 11, 12, 13, 14 } };
+            System.Console.WriteLine(arrayOut[0, 0]);
+        }
+             
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Figure f = new Figure();
+            //f.FigureSquare();
+            //f.FigureTriangle();
+            //f.FigureRectangle();
+            //f.SpaceTriangle();
+            //f.InSpaceTriangle();
+            //OutStarsTriangle Ost = new OutStarsTriangle();
+            //Ost.OutStars();
+            Array ar = new Array();
+            ar.ArrayOutput();
+            Console.ReadLine();
+        }
+
+    }
+
+}
